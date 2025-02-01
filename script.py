99cb8e14776d2b5b34bccc67f4e09b9c744c7638
fatiha-51 <boudehanefatiha51@gmail.com>
2025-02-01 09:03:31 +0100

    Delete script.py

--- script.py
+++
@@ -1,8 +0,0 @@
-@app.route('/generate-audio', methods=['POST'])
-def generate_audio():
-    data = request.get_json()
-    nom = data.get('nom')
-    sujet = data.get('sujet')
-    contenu = data.get('contenu')
-    # Gérer la génération d'audio ici
-    return jsonify({"message": "Audio généré avec succès"}), 200
